5f232586affc87710413f9b14d023224960f3796
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-04 10:18:56 +0100

    Insurance Company Project

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
 import Banner from "@/components/sections/Banner";
-import Layout from "../components/layout/Layout"
+import Layout from "@/components/layout/Layout"
+import Brand from "@/components/sections/Brand";
+import About from "@/components/sections/About";
 
 export default function Home() {
   return (
-    <>
-            <Layout headerStyle={1} footerStyle={1}>
-              <Banner/>    
-            </Layout>
+    <div className="homestyle2">
+        <Layout headerStyle={1} footerStyle={1}>
+              <Banner/>
+              <Brand/> 
+              <About/>  
+        </Layout>
 
-        </>
+    </div>
   );
 }
